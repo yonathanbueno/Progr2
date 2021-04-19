07abb039ec59c702c228429eac6352a951630b1c
Yonathan Bueno <78409767+yonathanbueno@users.noreply.github.com>
2021-04-19 16:06:42 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Progr2!");
+        }
+    }
+}
